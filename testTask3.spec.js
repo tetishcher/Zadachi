6ad10169091c60f672106ab4c903f42e8886cc8e
Tetiana Shcherbata <36457587+tetishcher@users.noreply.github.com>
2018-08-31 09:04:07 +0300

    Update testTask3.spec.js

--- testTask3.spec.js
+++ testTask3.spec.js
@@ -1,16 +1,5 @@
+const getNumbers = require('./task3');
 describe("A suite is just a function", function () {
-const getNumbers = arr => {
-  let count = 0;
-  for (let i = 0; i < arr.length; i += 1) {
-    const a = arr[i] % 2;
-    if (a === 0) {
-      count += 1;
-    }
-  }
-  return count;
-};
-module.exports = getNumbers;
-console.log(getNumbers(process.argv.slice(2)));
 
     it("and so is a spec", function () {
         expect(getNumbers(5)).toEqual([3, 4, 5]);
