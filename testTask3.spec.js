431281d5d2ce1b6558963b29d4519e69daa8938f
Tetiana Shcherbata <tetiana.s.shcherbata@gmail.com>
2018-08-31 05:51:09 +0000

    1

--- testTask3.spec.js
+++ testTask3.spec.js
@@ -1,5 +1,16 @@
-const getNumbers = require('task3.js');
 describe("A suite is just a function", function () {
+const getNumbers = arr => {
+  let count = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    const a = arr[i] % 2;
+    if (a === 0) {
+      count += 1;
+    }
+  }
+  return count;
+};
+module.exports = getNumbers;
+console.log(getNumbers(process.argv.slice(2)));
 
     it("and so is a spec", function () {
         expect(getNumbers(5)).toEqual([3, 4, 5]);
