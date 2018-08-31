edc2cea7a006640a69edc0aeaff93d66bb305255
Tetiana Shcherbata <36457587+tetishcher@users.noreply.github.com>
2018-08-31 09:02:31 +0300

    Update testTask2.spec.js

--- testTask2.spec.js
+++ testTask2.spec.js
@@ -1,14 +1,5 @@
+const getNumbers = require('./task2');
 describe("A suite is just a function", function () {
-    const getNumbers = arr => {
-  let count = 0;
-  for (let i = 0; i < arr.length; i += 1) {
-    const a = arr[i] % 2;
-    if (a === 0) {
-      count += 1;
-    }
-  }
-  return count;
-};
 
     it("and so is a spec", function () {
 
